6cb031c5f95d7394cf8953199e18c41c868e302a
Grace Thompson <grace.thompson97@gmail.com>
2020-12-11 16:09:34 +0000

    fixed!!!

--- torahreader.php
+++ torahreader.php
@@ -175,149 +175,110 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
  elseif ($parasha == 'Bereshit' && $aliyah == '7' && $cycle == 'Annual') {
  $verses = 'Genesis.5.25-6.8';
  }
- //elseif ($parasha == 'Noach' && $aliyah == '1' && $cycle == 'Annual') {
- //$verses = 'Genesis.6.9-22';
- //}
- //elseif ($parasha == 'Noach' && $aliyah == '2' && $cycle == 'Annual') {
- //$verses = 'Genesis.7.1-16';
- //}
- //elseif ($parasha == 'Noach' && $aliyah == '3' && $cycle == 'Annual') {
- //$verses = 'Genesis.7.17-8.14';
- //}
- //elseif ($parasha == 'Noach' && $aliyah == '4' && $cycle == 'Annual') {
- //$verses = 'Genesis.8.15-9.7';
- //}
- //elseif ($parasha == 'Noach' && $aliyah == '5' && $cycle == 'Annual') {
- //$verses = 'Genesis.9.8-17';
-// }
-// elseif ($parasha == 'Noach' && $aliyah == '6' && $cycle == 'Annual') {
-// $verses = 'Genesis.9.18-10.32';
-// }
-// elseif ($parasha == 'Noach' && $aliyah == '7' && $cycle == 'Annual') {
-// $verses = 'Genesis.11.1-32';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '1' && $cycle == 'Annual') {
-// $verses = 'Genesis.12.1-13';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '2' && $cycle == 'Annual') {
-// $verses = 'Genesis.12.14-13.4';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '3' && $cycle == 'Annual') {
-// $verses = 'Genesis.13.5-18';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '4' && $cycle == 'Annual') {
-// $verses = 'Genesis.14.1-20';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '5' && $cycle == 'Annual') {
-// $verses = 'Genesis.14.21-15.6';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '6' && $cycle == 'Annual') {
-// $verses = 'Genesis.15.7-17.6';
-// }
-// elseif ($parasha == 'Lech Lecha' && $aliyah == '7' && $cycle == 'Annual') {
-// $verses = 'Genesis.17.7-27';
-// }
- curl_setopt($ch, CURLOPT_URL, 'http://www.sefaria.org/api/texts/' . $verses . '?context=0');
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
- $data = curl_exec($ch);
- $array = json_decode($data, true);
- $hebrew = $array['he'];
- $english = $array['text'];
- function subArraysToString($ar, $sep = "<br> <br>") {
- $str = '';
- foreach ($ar as $val) {
- $str .= implode($sep, $val);
- $str .= $sep;
- }
- $str = rtrim($str, $sep);
- return $str;
- }
- $hebrewString = subArraysToString($hebrew);
- $englishString = subArraysToString($english);
- $pattern = "/-\d+$/i";
- if (preg_match($pattern, $verses)) {
- $hebrewString = implode("<br> <br>", $hebrew);
- $englishString = implode("<br> <br>", $english);
- }
- $_SESSION['heb'] = $hebrewString;
- $_SESSION['eng'] = $englishString;
+
+  curl_setopt($ch, CURLOPT_URL, 'http://www.sefaria.org/api/texts/' . $verses . '?context=0');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+   $data = curl_exec($ch);
+    $array = json_decode($data, true);
+    $hebrew = $array['he'];
+     $english = $array['text'];
+     function subArraysToString($ar, $sep = "<br> <br>") {
+	      $str = '';
+	       foreach ($ar as $val) {
+		        $str .= implode($sep, $val);
+			 $str .= $sep;
+			 }
+	       $str = rtrim($str, $sep);
+	       return $str;
+	        }
+      $hebrewString = subArraysToString($hebrew);
+      $englishString = subArraysToString($english);
+       $pattern = "/-\d+$/i";
+       if (preg_match($pattern, $verses)) {
+	        $hebrewString = implode("<br> <br>", $hebrew);
+		 $englishString = implode("<br> <br>", $english);
+		 }
+        $_SESSION['heb'] = $hebrewString;
+        $_SESSION['eng'] = $englishString;
 ?>
 <?php
- $region = 'eastus';
- $AccessTokenUri = "https://".$region.".api.cognitive.microsoft.com/sts/v1.0/issueToken";
- $apiKey = 'fb5158e7a2b24abab5a6fa73ae9287e2';
 
- $options = array(
- 'http' => array(
- 'header' => "Ocp-Apim-Subscription-Key: ".$apiKey."\r\n" .
- "content-length: 0\r\n",
- 'method' => 'POST',
- ),
- );
+	$region = 'eastus';
+	$AccessTokenUri = "https://".$region.".api.cognitive.microsoft.com/sts/v1.0/issueToken";
+	$apiKey = '0e1e79d9fbdb4e01b851fdafee6f6c0f';
 
- $context = stream_context_create($options);
+$options = array(
+    'http' => array(
+        'header'  => "Ocp-Apim-Subscription-Key: ".$apiKey."\r\n" .
+        "content-length: 0\r\n",
+        'method'  => 'POST',
+    ),
+);
 
- $access_token = file_get_contents($AccessTokenUri, false, $context);
+$context  = stream_context_create($options);
 
- if (!$access_token) {
- throw new Exception("Problem with $AccessTokenUri, $php_errormsg");
- }
- else{
+$access_token = file_get_contents($AccessTokenUri, false, $context);
 
- $ttsServiceUri = "https://".$region.".tts.speech.microsoft.com/cognitiveservices/v1";
+if (!$access_token) {
+    throw new Exception("Problem with $AccessTokenUri, $php_errormsg");
+  }
+else{
 
- $doc = new DOMDocument();
+   $ttsServiceUri = "https://".$region.".tts.speech.microsoft.com/cognitiveservices/v1";
 
- $root = $doc->createElement( "speak" );
- $root->setAttribute( "version" , "1.0" );
- $root->setAttribute( "xml:lang" , "he-IL" );
+   $doc = new DOMDocument();
 
- $voice = $doc->createElement( "voice" );
- $voice->setAttribute( "xml:lang" , "he-IL" );
- $voice->setAttribute( "xml:gender" , "Male" );
- $voice->setAttribute( "name" , "he-IL-Asaf");
+   $root = $doc->createElement( "speak" );
+   $root->setAttribute( "version" , "1.0" );
+   $root->setAttribute( "xml:lang" , "he-IL" );
+
+   $voice = $doc->createElement( "voice" );
+   $voice->setAttribute( "xml:lang" , "he-IL" );
+   $voice->setAttribute( "xml:gender" , "Male" );
+   $voice->setAttribute( "name" , "he-IL-Asaf"); 
  $prosody = $doc->createElement( "prosody" );
- $prosody->setAttribute( "rate", "x-slow" );
- $text = $doc->createTextNode( $_SESSION['heb'] );
- $prosody->appendChild( $text );
- $voice->appendChild( $prosody );
- $root->appendChild( $voice );
- $doc->appendChild( $root );
- $data = $doc->saveXML();
+    $prosody->setAttribute( "rate", "x-slow" );
+    $text = $doc->createTextNode( $_SESSION['heb'] );
+     $prosody->appendChild( $text );
+     $voice->appendChild( $prosody );
+      $root->appendChild( $voice );
+      $doc->appendChild( $root );
+       $data = $doc->saveXML();
 
- $options = array(
- 'http' => array(
- 'header' => "Content-type: application/ssml+xml\r\n" .
- "X-Microsoft-OutputFormat: audio-16khz-128kbitrate-mono-mp3\r\n" .
- "Authorization: "."Bearer ".$access_token."\r\n" .
- "X-Search-AppId: 07D3234E49CE426DAA29772419F436CA\r\n" .
- "X-Search-ClientID: 1ECFAE91408841A480F00935DC390960\r\n" .
- "User-Agent: TTSPHP\r\n" .
- "content-length: ".strlen($data)."\r\n",
- 'method' => 'POST',
- 'content' => $data,
+   $options = array(
+    'http' => array(
+        'header'  => "Content-type: application/ssml+xml\r\n" .
+                    "X-Microsoft-OutputFormat: riff-24khz-16bit-mono-pcm\r\n" .
+                    "Authorization: "."Bearer ".$access_token."\r\n" .
+                    "X-Search-AppId: 07D3234E49CE426DAA29772419F436CA\r\n" .
+                    "X-Search-ClientID: 1ECFAE91408841A480F00935DC390960\r\n" .
+                    "User-Agent: TTSPHP\r\n" .
+                    "content-length: ".strlen($data)."\r\n",
+        'method'  => 'POST',
+        'content' => $data,
+        ),
+    );
 
- ),
+    $context  = stream_context_create($options);
 
- );
+    $result = file_get_contents($ttsServiceUri, false, $context);
+    if (!$result) {
+        throw new Exception("Problem with $ttsServiceUri, $php_errormsg");
+      }
+    else{
+	 $audioFile = md5($result) . '.wav';
+	 $torahAudio = glob("*.wav");
+	  rename($torahAudio[0], './torah.wav');
+	  $status = file_put_contents('./' . 'torah.wav', $result);
 
- $context = stream_context_create($options);
+    }
+}
 
- $result = file_get_contents($ttsServiceUri, false, $context);
- if (!$result) {
- throw new Exception("Problem with $ttsServiceUri, $php_errormsg");
- }
- else{
- $audioFile = md5($result) . '.wav';
- $torahAudio = glob("*.wav");
- rename($torahAudio[0], './torah.wav');
- $status = file_put_contents('./' . 'torah.wav', $result);
- }
+?>  
 
-
- }
-?>
+</body>
+</html>
 <div class="row">
  <div class="column">
  <div class="hebrew-column">
@@ -344,3 +305,4 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 </div>
 </body>
 </html>
+
